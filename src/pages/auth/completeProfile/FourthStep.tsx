2c3993ecad671a26a21f2eaf40b67430b286809c
mahdidelavarz <mdelavar903@gmail.com>
2025-03-30 14:50:38 +0330

    third step added

--- src/pages/auth/completeProfile/FourthStep.tsx
+++ src/pages/auth/completeProfile/FourthStep.tsx
@@ -1,11 +1,11 @@
-import AuthLayout from '../../../components/layout/AuthLayout'
+import AuthLayout from "../../../components/layout/AuthLayout";
 
 function FourthStep() {
   return (
     <AuthLayout isBack={false}>
-    <div></div>
-  </AuthLayout>
-  )
+      <div className="w-full flex flex-col justify-center items-center mt-14"></div>
+    </AuthLayout>
+  );
 }
 
-export default FourthStep
+export default FourthStep;
--- src/pages/auth/completeProfile/ThirdStep.tsx
+++ src/pages/auth/completeProfile/ThirdStep.tsx
@@ -1,12 +1,35 @@
+import { useEffect } from "react";
 import AuthLayout from "../../../components/layout/AuthLayout";
+import CircleProgress from "../../../components/ui/CircleProgress";
 import useProgressStore from "../../../store/ProfileProgressStore";
+import { useNavigate } from "react-router-dom";
 
 function ThirdStep() {
-  const { step } = useProgressStore();
-  console.log(step);
+  const navigate = useNavigate();
+  const { step, setStep } = useProgressStore();
+
+  useEffect(() => {
+    setStep(3);
+  }, []);
+
   return (
     <AuthLayout isBack={false}>
-      <div></div>
+      <div className="w-full flex flex-col justify-center items-center mt-14">
+        <h1 className="text-sm">جنسیت خود را انتخاب کنید.</h1>
+        <div className="flex gap-6 mt-32">
+          <img src="../../../../public/images/2618750e-d6d0-4922-9fec-aa06dc0d641c.png" alt="" />
+          <img src="../../../../public/images/Group 1261153137.png" className="w-28 h-28" alt="" />
+        </div>
+        <div className="w-full flex items-center gap-2 mt-32">
+          <CircleProgress step={step} />
+          <button
+            className="btn-full"
+            onClick={() => navigate("/completeProfile/secondStep")}
+          >
+            ثبت و مرحله بعد
+          </button>
+        </div>
+      </div>
     </AuthLayout>
   );
 }

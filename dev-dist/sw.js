8af7d110a29f342ecd44bb8a6e6a996ee7490164
mdelavar903@gmail.com <mdelavar903@gmail.com>
2025-04-01 13:15:53 +0330

    extra files deleted

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fbaa1ibdq18"
+    "revision": "0.8j6og0qlai8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
